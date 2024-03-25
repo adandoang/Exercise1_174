ee2493454c82561bd3b86c5d12d7fc2aac0acd1f
adandoang <akhdanjzif@gmail.com>
2024-03-25 10:50:42 +0700

    merubah notif

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 function showNotification() {
-    // Periksa apakah browser mendukung API Notifikasi
-    if ('Notification' in window) {
-        Notification.requestPermission().then(function(permission) {
-            if (permission === 'granted') {
-                // Tampilkan notifikasi
-                var notification = new Notification('Notifikasi', {
-                    body: 'KERJA BAGUS.',
-                    icon: 'solat.jpeg'
-                });
-            }
-        });
-    } else {
-        alert('Browser tidak mendukung Notifikasi.');
-    }
+    alert('KERJA BAGUS BANG.');
 }
 
 document.getElementById('tombol').addEventListener('click', showNotification);
